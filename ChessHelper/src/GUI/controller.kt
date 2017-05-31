64763c2d778aed688bbc06e210bf00c72f1c0e74
Vvabi <fabian.lenhardt@onlinehome.de>
2017-05-31 23:24:03 +0200

    More gui stuff.

--- ChessHelper/src/GUI/controller.kt
+++ ChessHelper/src/GUI/controller.kt
@@ -3,8 +3,13 @@ package GUI
 /**
  * Created by vabi on 31.05.17.
  */
+import EngineHandling.ChessEngine
+import Tests.runPerftTests
 import javafx.fxml.FXML
+import javafx.scene.control.Button
 import javafx.scene.control.Label
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
 
 class controller {
 
@@ -13,4 +18,34 @@ class controller {
     @FXML fun handleClickMe() {
         helloLabel?.text = "You clicked!!!"
     }
+
+    @FXML var perft: Button? = null
+
+    var perftRunning: AtomicBoolean = AtomicBoolean(false)
+
+    @FXML fun handlePerftTests() {
+        if(perftRunning.compareAndSet(false, true)) {
+            perft?.setStyle("-fx-base: #ffff00;");
+            thread() {
+                perft()
+            }
+        } else {
+            println("Tests are already running, not starting again")
+        }
+    }
+
+    fun perft() {
+        var engine = ChessEngine("/home/vabi/code/chessEngine/Cpp/Release/Vabi", "/home/vabi")
+        var ret = runPerftTests(engine)
+        engine.close()
+        println(ret)
+
+        if(ret) {
+            perft?.setStyle("-fx-base: #00ff00;")
+        } else {
+            perft?.setStyle("-fx-base: #ff0000;")
+        }
+        perftRunning.set(false)
+    }
+
 }
--- ChessHelper/src/Tests/Perft.kt
+++ ChessHelper/src/Tests/Perft.kt
@@ -54,6 +54,6 @@ fun runPerftTests(engine: ChessEngine): Boolean {
         }
 
     }
-    engine.close()
+
     return res
 }
