48fcc7199862c0abb97065b8a83b7c46ba28265c
VVabi <fabian.lenhardt@onlinehome.de>
2018-12-29 18:15:04 +0100

    lint issue

--- Cpp/src/lib/Evaluation/space/space.cpp
+++ Cpp/src/lib/Evaluation/space/space.cpp
@@ -17,7 +17,6 @@
 int16_t evals[] = {0, 5, 10, 18, 30, 45, 60, 75, 90};
 
 EvalComponentResult spaceAdvantage(const chessPosition* position, const evalParameters* evalParams __attribute__((unused)), EvalMemory* evalMemory  __attribute__((unused))) {
-
     EvalComponentResult ret;
 
     int32_t numWhitePieces = popcount(position->pieces[white] & NORTHHALF);
@@ -28,8 +27,7 @@ EvalComponentResult spaceAdvantage(const chessPosition* position, const evalPara
 
     ret.early_game = 0;
     ret.common = (evals[numWhitePieces]-evals[numBlackPieces])/3;
-//#ifdef EXPERIMENTAL
     ret.endgame = (evals[numWhitePieces]-evals[numBlackPieces])/3;
-//#endif
+
     return ret;
 }
